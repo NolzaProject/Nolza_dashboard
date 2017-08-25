3d270e2343c95ce94c8fa5ec96f6f2a239a0ad61
sauber92 <jjy920517@gmail.com>
2017-08-25 20:45:31 +0900

    [Nolza-5] 프로젝트 정리 및 README 작성

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var flash = require('connect-flash');
+var asyc = require('async');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,9 +25,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use(session({secret:'!@#$NOLZADASHBOARD!@#$'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', index);
-app.use('/users', users);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var dbconfig = require('../config/database.js');
+var connection = mysql.createConnection(dbconfig);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+
 });
 
 module.exports = router;
